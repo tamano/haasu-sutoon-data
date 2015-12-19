de9731a9a8ec5041a1da71913a8803058040d2a4
Yuya TAMANO <everfree.main@gmail.com>
2015-12-19 19:10:05 +0900

    Remove useless things

--- lib/haasu-sutoon-data.rb
+++ lib/haasu-sutoon-data.rb
@@ -2,6 +2,3 @@ require 'thor'
 
 require 'haasu-sutoon-data/cli'
 require 'haasu-sutoon-data/version'
-
-module HaasuSutoonData
-end
--- lib/haasu-sutoon-data/cli.rb
+++ lib/haasu-sutoon-data/cli.rb
@@ -3,13 +3,11 @@ class HaasuSutoonData::CLI < Thor
     'generate {out_file}',
     'Access to Hearthstonejson.com and output English-Japanese cards list in JS Hash style named CardList.'
   def export(out_file)
-    #error_message = MboxHeaderScraper::Scraper.check_in_file(in_file)
     if error_message
       puts "input file error: #{error_message}"
       exit 1
     end
 
-    #error_message = MboxHeaderScraper::Scraper.check_out_file(out_file)
     if error_message
       puts "output file error: #{error_message}"
       exit 1
