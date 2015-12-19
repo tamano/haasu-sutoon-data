ba68ea7eeee86ec7fe25026ca11eff2d622e146f
Yuya TAMANO <everfree.main@gmail.com>
2015-12-19 19:01:30 +0900

    Setup thor

---
+++ lib/haasu-sutoon-data.rb
@@ -0,0 +1,7 @@
+require 'thor'
+
+require 'haasu-sutoon-data/cli'
+require 'haasu-sutoon-data/version'
+
+module HaasuSutoonData
+end
--- lib/haasu-sutoon-data/cli.rb
+++ lib/haasu-sutoon-data/cli.rb
@@ -1,5 +1,19 @@
-require "haasu-sutoon-data/version"
+class HaasuSutoonData::CLI < Thor
+  desc \
+    'generate {out_file}',
+    'Access to Hearthstonejson.com and output English-Japanese cards list in JS Hash style named CardList.'
+  def export(out_file)
+    #error_message = MboxHeaderScraper::Scraper.check_in_file(in_file)
+    if error_message
+      puts "input file error: #{error_message}"
+      exit 1
+    end
 
-module HaasuSutoonData
-  # Your code goes here...
+    #error_message = MboxHeaderScraper::Scraper.check_out_file(out_file)
+    if error_message
+      puts "output file error: #{error_message}"
+      exit 1
+    end
+
+  end
 end
