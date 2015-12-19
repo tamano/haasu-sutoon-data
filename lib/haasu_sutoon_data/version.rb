6f698d253770698dbf3a195c10700787aedcd830
Yuya TAMANO <everfree.main@gmail.com>
2015-12-19 19:50:44 +0900

    Implement HaasuSutoonData.file_id

--- lib/haasu_sutoon_data/version.rb
+++ lib/haasu_sutoon_data/version.rb
@@ -1,4 +1,8 @@
 # represent version
 module HaasuSutoonData
   VERSION = '0.1.0'
+
+  def self.file_id
+    "#{HaasuSutoonData::VERSION}/#{Time.now.strftime('%Y%m%d%H%M%S')}"
+  end
 end
--- spec/haasu_sutoon_data_spec.rb
+++ spec/haasu_sutoon_data_spec.rb
@@ -8,4 +8,14 @@ describe HaasuSutoonData do
     it { expect(subject).not_to be_nil }
   end
 
+  describe 'file_id' do
+    subject { hsd.file_id }
+
+    before do
+      stub_const("#{hsd}::VERSION", '9.9.9')
+      allow(Time).to receive(:now).and_return(Time.local(2015, 12, 19, 19, 35, 12))
+    end
+
+    it { expect(subject).to eq('9.9.9/20151219193512') }
+  end
 end
